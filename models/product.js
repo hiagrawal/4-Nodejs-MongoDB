7c76d7b7fe8052be72e8768beaa3ff106db27521
hiagrawal <agrawal.hina13@gmail.com>
2021-10-06 19:17:56 +0530

    creating the database connection and creating products

--- models/product.js
+++ models/product.js
@@ -1,27 +1,28 @@
-const Sequelize = require('sequelize');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, description, imageUrl){
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
   }
-});
+
+  //in Mongodb, we have database then collections then documents
+  //database is 'shop' we defined in database.js file
+  //collection is 'products'. If it has the collection will write to it, it not then will create one
+  //insertOne is method which we can find in mongoDb docs: https://docs.mongodb.com/manual/crud/
+  //it takes object so insertOne({name: 'A book', price: 10.00}) ..
+  //but since we have all this in 'this', we can directly pass the same
+  save(){
+    const db = getDb();
+    return db.collection('products').insertOne(this)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => console.log(err));
+
+  }
+}
 
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,14 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 //we are using callback here bcz we need to call this function in appjs and start server only when connected to database
+//IN the link, 'shop' is the database here, so if it exists, it will write to it else will create one
 const mongoConnect = callback => {
-  MongoClient.connect('mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  MongoClient.connect('mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/shop?retryWrites=true&w=majority')
   .then(client => {
     console.log('Connected to MongoDB server!');
-    callback(client);
+    _db = client.db();
+    callback();
   })
   .catch(err => console.log(err));
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if(_db){
+    return _db;
+  }
+  throw 'No Database Found!';
+
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
