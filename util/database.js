4e033c9c8c7a2ced3133e690f7f861a6cd35faa6
hiagrawal <agrawal.hina13@gmail.com>
2021-10-06 17:45:52 +0530

    Setting up the MongoDB Application

---
+++ util/database.js
@@ -0,0 +1,19 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+//we are using callback here bcz we need to call this function in appjs and start server only when connected to database
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Connected to MongoDB server!');
+    callback(client);
+  })
+  .catch(err => console.log(err));
+}
+
+module.exports = mongoConnect;
+
+//mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/PROJECT 0?retryWrites=true&w=majority
+//mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/cluster0?retryWrites=true&w=majority
+//mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/test?retryWrites=true&w=majority
+//mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
