4b4d8feb5f8047715f86811aa6882f0e21f2aff2
hiagrawal <agrawal.hina13@gmail.com>
2021-10-07 13:03:07 +0530

    Fixing the Add functionality

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongodb.ObjectId(id);
+    this._id = id ? new mongodb.ObjectId(id) : null ;
   }
 
   //in Mongodb, we have database then collections then documents
