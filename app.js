dc621af182979f457e68fa9babdbe31754f0910a
hiagrawal <agrawal.hina13@gmail.com>
2021-10-09 16:19:08 +0530

    adding a user model and creating a user

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const errorController = require('./controllers/error');
 //const mongoConnect = require('./util/database').mongoConnect;
 const mongoose = require('mongoose');
 
-//const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -21,6 +21,7 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//when using mongo
 //created the user manually in db and fetching that user by id
 // app.use((req, res, next) => {
 //   User.findById('615eab1fc7caee6b996ad1c4')
@@ -35,18 +36,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     .catch(err => console.log(err));
 // });
 
+//when using mongoose
+//created the user at the connect and fetching that user by id
+//findById is a method given by mongoose and user that we get in result will have access to all mongoose methods also
+app.use((req, res, next) => {
+  User.findById('6161733cd2ec6bef280e800f')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+//when using mongo
 // mongoConnect(() => {
 //   app.listen(3000);
 // })
 
+//when using mongoose
+//findOne will return the first user it finds in users collection
 mongoose.connect('mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/shopMongoDb?retryWrites=true&w=majority')
 .then(result => {
   console.log('Connect to Mongo DB using Mongoose!!');
+  User.findOne().then(user => {
+    if(!user){
+      const user = new User({
+        name: 'Hina',
+        email: 'test@test.com',
+        cart: {
+          items: []
+        }
+      });
+      user.save();
+    }
+  });
   app.listen(3000);
 })
 .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+//when using mongo
 /*const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
@@ -109,3 +110,30 @@ class User {
 }
 
 module.exports = User;*/
+
+//when using mongoose
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type:String,
+    required:true
+  },
+  email: {
+    type:String,
+    required:true
+  },
+  cart: {
+    items: [
+      {
+        productId: {type: Schema.Types.ObjectId , required:true},
+        quantity: {type: Number, required: true}
+      }
+    ]
+  }
+
+});
+
+module.exports = mongoose.model('User', userSchema);
